9049cd7f6567d060230a06ef6f94572f6312bc92
vtjiong <vtjiong@bu.edu>
2024-07-18 23:03:40 +0800

    Final version

---
+++ main.py
@@ -0,0 +1,37 @@
+import requests
+from datetime import datetime
+application_id= "Your Application ID"
+application_key = "Your Application Key"
+endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+headers = {
+    'x-app-id' : application_id,
+    'x-app-key': application_key,
+    'x-remote-user-id': "0"
+}
+parameters = {
+    "query": input("Tell me which exercises you did: "),
+    "gender": "Male",
+    "weight_kg": "105",
+    "height_cm": "186",
+    "age": 21
+}
+response = requests.post(url=endpoint, headers=headers, json=parameters)
+result = response.json()["exercises"]
+print(result)
+sheety_endpoint="Your End Point"
+for x in result:
+    workout = {
+       "workout": {
+            "date":datetime.now().strftime("%m/%d/%Y"),
+            "time":datetime.now().strftime("%H:%M:%S"),
+            "exercise":x["name"].title(),
+            "duration":x["duration_min"],
+            "calories":x["nf_calories"]
+        }
+    }
+    response = requests.post(url=sheety_endpoint, json=workout)
+
+
+
+# response.json() parses the response from strings to a valid json data structure using a lists and stuff,
+# while response.text is still a string
